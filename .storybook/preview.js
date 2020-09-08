a35ad700f76553e90683a6e6d00f0fec5869bb10
maximiliangustmann <maxigustmann@wtnet.de>
2020-09-08 15:04:58 +0200

    React Router Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,10 @@
+import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
 }
+
+export const decorators = [
+  (storyFn) => <MemoryRouter>{storyFn()}</MemoryRouter>,
+]
--- src/components/CreateButton.stories.js
+++ src/components/CreateButton.stories.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import CreateButton from './CreateButton'
-
 export default {
   title: 'CreateButton',
   component: CreateButton,
