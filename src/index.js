8b885943ea6ee9ad82d518f5bdc0f5e6f905d13f
maximiliangustmann <maxigustmann@wtnet.de>
2020-09-18 15:22:02 +0200

    Setup GlobalStyles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter as Router } from 'react-router-dom'
+import GlobalStyles from './services/GlobalStyles'
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <Router>
       <App />
     </Router>
---
+++ src/services/GlobalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+export default createGlobalStyle`
+* {
+    box-sizing: border-box;
+    font-family: "Open Sans", sans-serif;
+}
+body {
+    font-size: 112.5%;
+    background: #F4F4F4;
+    margin: 0;
+    padding: 0;
+    
+}
+
+
+`
