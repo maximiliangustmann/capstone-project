6d7ce2edddc483acdc6d5027d9b2ee68fcb02baf
maximiliangustmann <maxigustmann@wtnet.de>
2020-09-22 16:47:07 +0200

    Update Snapshots

--- cypress/integration/edit_review.spec.js
+++ cypress/integration/edit_review.spec.js
@@ -39,7 +39,7 @@ context('Edit review', () => {
     cy.get('label')
       .contains('Category')
       .find('select')
-      .should('have.value', 'film')
+      .should('have.value', 'Film')
 
     cy.get('label')
       .contains('Subcategory')
