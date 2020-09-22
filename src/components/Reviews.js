67e6a553fd0356cc57e8e61c80a8f2276eeb5075
maximiliangustmann <maxigustmann@wtnet.de>
2020-09-22 10:40:31 +0200

    Copy Dennis filter function

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Filter from './Filter'
 import Review from './Review'
 
 export default function Reviews({ reviews, onRemove, onEdit }) {
+  const filters = ['Show all', 'book', 'audiobook', 'podcast', 'film', 'series']
+  const [active, setActive] = useState(filters[0])
   return (
     <>
-      {reviews.map((review) => {
-        return (
+      <Filter filters={filters} active={active} setActive={setActive} />
+      {reviews
+        ?.filter(
+          (reviews) =>
+            active === 'Show all' || !reviews.category.includes(active)
+        )
+        .map((review) => (
           <Review
             key={review.id}
             {...review}
             onRemove={onRemove}
             onEdit={onEdit}
           />
-        )
-      })}
+        ))}
     </>
   )
 }
