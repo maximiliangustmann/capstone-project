b065186410f6d4f678290c6a4e9fa8f5a15b1376
maximiliangustmann <maxigustmann@wtnet.de>
2020-09-27 13:34:59 +0200

    Create SortButton

---
+++ src/components/SortButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SortButton() {
+  return (
+    <select>
+      <option value="subcategory">Subcategory</option>
+      <option value="rating">Rating</option>
+    </select>
+  )
+}
